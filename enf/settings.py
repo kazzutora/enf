49a49324cc952564a31702827616dff5843479c5
kazzutora <diabloroale9@gmail.com>
2026-04-11 22:16:50 +0200

    ending functions cart.py

--- enf/settings.py
+++ enf/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'cart',
 
 ]
 
@@ -139,3 +140,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 86400 # 
+SESSION_SAVE_EVERY_REQUEST = True
